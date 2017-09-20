a4eab1e81931b57fcb6a3137731a85eeeb6ba803
Kim Jin Su <item4_hun@hotmail.com>
2017-09-21 00:37:22 +0900

    Add missing deps

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import find_packages, setup
 
 install_requires = {
+    # Django Things
     'django[argon2] >= 1.11.5',
     'django-timezone-field >= 2.0',
+    'django-allauth >= 0.33.0',
 }
 
 tests_require = {
     'mypy >= 0.521',
     'pytest >= 3.2.1',
+    'pytest-django >= 3.1.2',
 }
 
 extras_require = {
