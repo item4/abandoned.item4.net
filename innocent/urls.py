3d3eb588ea55bbcbd1d2b99a6922ab4ed317fc77
Kim Jin Su <item4_hun@hotmail.com>
2017-09-23 00:38:45 +0900

    Drop defalut/allauth auth logic

--- innocent/urls.py
+++ innocent/urls.py
@@ -17,8 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^a/', include('django.contrib.auth.urls')),
-    url(r'^accounts/', include('allauth.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^', include('api.urls')),
 ]
