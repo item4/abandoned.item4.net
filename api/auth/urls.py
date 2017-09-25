df6447567d447c2d7637545bd5f077091327f1d1
Kim Jin Su <item4_hun@hotmail.com>
2017-09-25 18:18:55 +0900

    Use correct cbv

--- api/auth/urls.py
+++ api/auth/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^login/$', views.Login.as_view(), name='login'),
-    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+    url(r'^logout/$', views.Logout.as_view(), name='logout'),
     url(r'^register/$', views.Register.as_view(), name='register'),
     url(
         r'^confirm/$',
